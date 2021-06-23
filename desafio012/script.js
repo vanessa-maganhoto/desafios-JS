35f918092a68a61d8d7fe265bc2f3a11aea89c63
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2021-06-23 16:21:41 -0300

    Melhorias no estilo

--- desafio012/script.js
+++ desafio012/script.js
@@ -10,12 +10,12 @@ function calcpreco(){
         let precof = 0
         precof = p2 - p1
         let aumento = (p2 * 100) / p1
-        let aumentofinal = aumento - 100
+        let aumentofinal = (aumento - 100).toFixed(2)
         resp.innerHTML = `<strong>Analisando os valores informados</strong> <br>\n O produto custava R$${p1} e agora custa R$${p2}.<br>\n Hoje o produto está mais caro. <br>\n O preço subiu R$${precof} em relação ao preço anterior. <br>\n Uma variação de ${aumentofinal}% pra cima.`
     } else {
         (p1 > p2)
         precof = p1 - p2
-        let dim = (p2 * 100) / p1
+        let dim = ((p2 * 100) / p1).toFixed(2)
         resp.innerHTML = `<strong>Analisando os valores informados</strong> <br>\n O produto custava R$${p1} e agora custa R$${p2}.<br>\n Hoje o produto está mais barato. <br>\n O preço caiu R$${precof} em relação ao preço anterior. <br>\n Uma variação de ${dim}% pra baixo.`
     }
 }
